25a87f225941730cd26a5c33bfb89cafd4c68359
IShamss <mohamedshams2000@gmail.com>
2021-03-18 17:00:53 +0200

    add user routes

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const postsRouter = require('./routes/posts');
 const reviewsRouter = require('./routes/reviews');
 
@@ -16,13 +16,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
 app.use('/posts/:id/reviews', reviewsRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,41 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next)=> {
   res.render('index', { title: 'Car - Store' });
+});
+router.get('/register', (req, res, next) =>{
+  res.send('Welcome to the register page')
+});
+router.post('/register', (req, res, next) =>{
+  res.send('post register add new user')
+});
+router.get('/login', (req, res, next) =>{
+  res.send('login user')
+});
+router.post('/login', (req, res, next) =>{
+  res.send('post login user')
+});
+router.get('/profile', (req, res, next) =>{
+  res.send('get user profile')
+});
+//to update user profile like username and password
+router.put('/profile/:user_id', (req, res, next) =>{
+  res.send('update user profile')
+});
+//forgot password
+router.get('/forgot', (req, res, next) =>{
+  res.send('I forgot my password')
+});
+router.put('/forgot', (req, res, next) =>{
+  res.send('I forgot my password update now')
+});
+
+router.get('/reset/:token', (req, res, next) =>{
+  res.send('reset my password')
+});
+router.put('/reset/:token', (req, res, next) =>{
+  res.send('reset my password put')
 });
 
 module.exports = router;
